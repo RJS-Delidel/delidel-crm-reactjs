037f22e0bbee209aab15ef710179ab656d41dd87
akmishra0725 <ayan.mishra@rjsfoods.com>
2024-10-28 23:28:06 +0530

    Added database file to connect databases

---
+++ delidel-crm/backend/model/user.js
@@ -0,0 +1,28 @@
+// backend/models/User.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  resetToken: {
+    type: DataTypes.STRING,
+  },
+  resetTokenExpiry: {
+    type: DataTypes.DATE,
+  },
+});
+
+module.exports = User;
